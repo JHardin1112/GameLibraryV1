859e6ea9ac6fc9bebfd10fbd1c28f33f79e176df
Jeremy Hardin <jeremyhardin@gmail.com>
2017-03-28 08:00:01 -0400

    First round of code after implementing EF Core

---
+++ GameLibraryV1/Models/Games.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameLibraryV1.Models
+{
+    public partial class Games
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Region { get; set; }
+        public string Name { get; set; }
+        public bool? Owned { get; set; }
+    }
+}
---
+++ GameLibraryV1/Models/GamesContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GameLibraryV1.Models
+{
+    public partial class GamesContext : DbContext
+    {
+        public virtual DbSet<Games> Games { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        //    optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\jhardin\Documents\Visual Studio 2017\Projects\GameLibraryV1\GameLibraryV1\Games.mdf"";Integrated Security=True;Connect Timeout=30;");
+        //}
+        public GamesContext(DbContextOptions<GamesContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Games>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnType("varchar(8)");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasColumnType("varchar(max)");
+
+                entity.Property(e => e.Owned)
+                    .HasColumnName("owned")
+                    .HasDefaultValueSql("0");
+
+                entity.Property(e => e.Region)
+                    .HasColumnName("region")
+                    .HasColumnType("nchar(10)");
+
+                entity.Property(e => e.Type)
+                    .HasColumnName("type")
+                    .HasColumnType("varchar(10)");
+            });
+        }
+    }
+}
